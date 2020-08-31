a52e8a383e9e0bd81d0ced84a0bde4425db77e12
Tristan <lordtharr@gmail.com>
2020-08-31 12:57:18 +1000

    Added subject details function get_data function now returns value parts for printing

--- Prac 04/subject_reader.py
+++ Prac 04/subject_reader.py
@@ -2,6 +2,7 @@
 CP1404/CP5632 Practical
 Data file -> lists program
 """
+from typing import List
 
 FILENAME = "subject_data.txt"
 
@@ -11,6 +12,7 @@ def main():
     print(data)
 
 
+
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
@@ -18,12 +20,18 @@ def get_data():
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
+        parts: List[str] = line.split(',')  # Separate the data into its parts
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        subject_details(parts)
     input_file.close()
+    return parts
+
+
+def subject_details(parts):
+    print("{} is taught by {} and has {} students".format(parts[0], parts[1], parts[2]))
 
 
 main()
