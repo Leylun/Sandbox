7a2e2578c05eb305eb2b2fcf5c429d8e704551e1
Tristan <lordtharr@gmail.com>
2020-10-18 15:24:54 +1000

    Added travel_tracker_2 Added travel_tracker_GUI Added placeCollection Added places and places_backup csv Added place class (All of place class and Travel_Tracker_GUI was removed) (Reinstalled all of Kivy and its pathing) (Impossible to access specific URL link on github) (Going to use sandbox location if it will work)

---
+++ CP1404_Assignment_2/place.py
@@ -0,0 +1,15 @@
+class place:
+    def __init__(self, name, country, priority, visited):
+        self.important = False
+        self.country = country
+        self.name = name
+        self.visited = visited
+        self.priority = priority
+        if self.priority <= 2:
+            self.important = True
+
+    def __str__(self):
+        if self.visited:
+            return "{} in {}, priority {} (visited)".format(self.name, self.country, self.priority)
+        elif not self.visited:
+            return "{} in {}, priority {}".format(self.name, self.country, self.priority)
